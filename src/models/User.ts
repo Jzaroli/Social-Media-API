cb97fdd9bfdd41426a99edcdffc3e7bf8a93170e
Jzaroli <jzaroli@gmail.com>
2025-01-13 13:06:13 -0700

    created models for User, Thought and Reaction

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,1 +1,45 @@
 import { Schema, Document, model, ObjectId } from 'mongoose';
+
+interface IUser extends Document {
+    username: string;
+    email: string;
+    thoughts?: ObjectId[];
+    friends?: ObjectId[];
+   }
+
+// Schema to create User model
+const userSchema = new Schema<IUser>(
+    {
+        username: String,
+        email: Number,
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'friend',
+            },
+        ],
+    },
+    {
+        // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
+        // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+        toJSON: {
+        virtuals: true,
+        },
+        id: false,
+    }
+);
+
+// Create a virtual property `friendCount` that gets the amount of friends per user
+userSchema.virtual('friendCount').get(function () {
+    return this.friends?.length;
+  });
+
+const User = model('user', userSchema);
+
+export default User
